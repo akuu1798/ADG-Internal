d87eff6e5352e7a0feeb8f1f7d75d428bf2ac576
Akshat Agarwal <akshat@Akshats-MacBook-Pro.local>
2017-07-06 01:43:30 +0530

    Starting

---
+++ ADG Internal/TripCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  TripCollectionCell.swift
+//  ADG Internal
+//
+//  Created by Akshat Agarwal on 06/07/17.
+//  Copyright © 2017 Akshat Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class TripCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet var workLabel:UILabel!
+    @IBOutlet var eventLabel:UILabel!
+    @IBOutlet var dateLabel:UILabel!
+}
